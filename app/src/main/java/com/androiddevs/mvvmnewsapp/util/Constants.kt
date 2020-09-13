1f5d79e6552a36fddb2eac3464a3698e7a652340
Igor Stevanovic <35437662+Giga99@users.noreply.github.com>
2020-09-13 13:11:16 +0200

    News listing app

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "93961b751a1a48b88d1cf6bb6eb788b0"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
